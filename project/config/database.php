6e05cdeb5463c393e98b2def5e72ac9f01547a7e
Mohamed Zayani <zayanimed97@gmail.com>
2021-03-25 11:51:05 +0100

    no message

--- project/config/database.php
+++ project/config/database.php
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => 'store',
+            'database' => 'proj2021',
             'username' => 'root',
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
